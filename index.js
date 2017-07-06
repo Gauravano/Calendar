4ed6bd42e179314c4c4e6728ddb62f99b60ce5ce
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-06 21:47:59 +0530

    all functionality added

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ var currentNum = 6;
 var currentMonth = 6;
 var currentYear = 2017;
 
-function month(currentMonth){
+
+function currentConfig(){
+	 currentNum = 6;
+ 	 currentMonth = 6;
+	 currentYear = 2017;	
+	 dateSetter()
+}
+
+function currentConfig(){
+	 currentNum = 6;
+ 	 currentMonth = 6;
+	 currentYear = 2017;	
+	 dateSetter(currentMonth,currentYear);
+}
+
+function nextMonthConfig(){
+	currentMonth += 1;
+	dateSetter(currentMonth,currentYear);
+}
+
+function prevMonthConfig(){
+	currentMonth -=1;
+	dateSetter(currentMonth,currentYear);
+}
+
+function nextYearConfig(){
+	currentYear += 1;
+	dateSetter(currentMonth,currentYear);
+}
+
+function prevYearConfig(){
+	currentYear -=1;
+	dateSetter(currentMonth,currentYear);
+}
+
+
+
+
+function monthConvert(currentMonth){
 	var monthStr;
 	switch(currentMonth){
 		case 0:monthStr = "January";
@@ -86,17 +124,22 @@ function numOfdays(){
 	}
 	return monthStr;	
 }
-var setdate = new Date('July 01, 2017 00:00:00');
-var setNum = setdate.getDate();
-var setDay = setdate.getDay();
 
-function dateSetter(month,year,numDays){
+
+function dateSetter(monthNum,year){
+
+	var month = monthConvert(monthNum);
+	var numDays = numOfdays(monthNum);
 
 	var fullDate = month + ' ' +'01, '+year+' 00:00:00';
 	console.log(fullDate);
 
 	var setdate = new Date(fullDate);
 	var setDay = setdate.getDay();
+
+	// var setdate = new Date('July 01, 2017 00:00:00');
+	// var setNum = setdate.getDate();
+	// var setDay = setdate.getDay();
 
 	for(var i=setDay,j=1;i<=34 && i<numDays;i++,j++){
 		document.getElementById(i).innerHTML = j;
@@ -107,3 +150,16 @@ function dateSetter(month,year,numDays){
 	}
 
 }
+
+var current = document.getElementById("current");
+var nextMonth =  document.getElementById("nextMonth");
+var PrevMonth =  document.getElementById("PrevMonth");
+var nextYear =  document.getElementById("nextYear");
+var PrevYear =  document.getElementById("PrevYear");
+
+current.addEventListener('click',currentConfig);
+nextMonth.addEventListener('click',nextMonthConfig);
+PrevMonth.addEventListener('click',prevMonthConfig);
+nextYear.addEventListener('click',nextYearConfig);
+PrevYear.addEventListener('click',prevYearConfig);
+
